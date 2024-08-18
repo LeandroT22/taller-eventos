7083df0267ca893d4a29661beb4a6a2b9d853308
LeandroT22 <trucidoleandro@gmail.com>
2024-08-18 15:25:04 -0300

    agrego alerta js

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let buttonhtml = document.getElementById("boton");
+
+  buttonhtml.addEventListener("click", function () {
+    alert("Hola! Soy el div");
+  });
+});
