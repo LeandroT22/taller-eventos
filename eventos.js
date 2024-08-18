88fe4211a5625792d8d2019494991d016717968a
LeandroT22 <trucidoleandro@gmail.com>
2024-08-18 16:20:24 -0300

    soluciono la propagación de sucesos con el evento onclick

--- eventos.js
+++ eventos.js
@@ -2,6 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   let buttonhtml = document.getElementById("boton");
 
   buttonhtml.addEventListener("click", function () {
+    buttonhtml.stopPropagation();
     alert("Hola! Soy el div");
   });
+
+  /* CODIGO NECESARIO EN CASO DE NECESITAR UN RETRASO ENTRE UNA ALERTA Y LA OTRA
+ buttonhtml.addEventListener("click", function () {
+    setTimeout(function() {
+        alert("Hola! Soy el div")
+      }, 1500);
+  });
+  */
 });
